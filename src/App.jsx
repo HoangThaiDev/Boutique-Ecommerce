7dddbb032f18b668a8036c5cb97a845b659ec461
HoangThaiDev <thaindev2000@gmail.com>
2024-10-24 11:32:08 +0700

    test deploy x1

--- src/App.jsx
+++ src/App.jsx
@@ -27,100 +27,65 @@ import ScrollTop from "./UI/ScrollTop";
 
 function App() {
   // Create + use Hooks
-  // const navigate = useNavigate();
-  // const location = useLocation();
-  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
 
   // Create + use variables
-  // const pathsAuth = ["/login", "/signup"];
+  const pathsAuth = ["/login", "/signup"];
 
   // Sides Effect
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     try {
-  //       const res = await APIServer.user.getUser();
-  //       const { isLoggedIn, accessToken, cart } = res.data;
-
-  //       // If client not logged in => keep going
-  //       if (res.status === 200 && !isLoggedIn) return;
-
-  //       // If client was logged in and lost accessToken => update new accessToken
-  //       if (res.status === 201 && isLoggedIn) {
-  //         return dispatch(
-  //           actionUser.save({
-  //             accessToken: accessToken,
-  //             isLoggedIn: isLoggedIn,
-  //             cart: cart,
-  //           })
-  //         );
-  //       }
-  //     } catch (error) {
-  //       const { data, status } = error.response;
-
-  //       if (status === 500) {
-  //         alert(data.message);
-  //         navigate("..");
-  //         return false;
-  //       }
-
-  //       if (status === 401) {
-  //         alert(data.message);
-  //         return dispatch(
-  //           actionUser.save({ accessToken: "", isLoggedIn: data.isLoggedIn })
-  //         );
-  //       }
-  //     }
-  //   };
-
-  //   // Check path not in Page Login & Register
-  //   if (!pathsAuth.includes(location.pathname)) {
-  //     fetchUser();
-  //   }
-  // }, []);
-  const [isLoading, setIsLoading] = useState(false);
-  const [products, setProducts] = useState([]);
   useEffect(() => {
-    console.log("Test:");
-    const fetchProduct = async () => {
+    const fetchUser = async () => {
       try {
-        const res = await APIServer.shop.getProducts();
-        console.log("Response received:", res);
-        if (res.statusText === "OK") {
-          const products = res.data;
-          console.log(res.data);
+        const res = await APIServer.user.getUser();
+        const { isLoggedIn, accessToken, cart } = res.data;
 
-          setProducts(products);
-          setIsLoading(true);
+        // If client not logged in => keep going
+        if (res.status === 200 && !isLoggedIn) return;
+
+        // If client was logged in and lost accessToken => update new accessToken
+        if (res.status === 201 && isLoggedIn) {
+          return dispatch(
+            actionUser.save({
+              accessToken: accessToken,
+              isLoggedIn: isLoggedIn,
+              cart: cart,
+            })
+          );
         }
       } catch (error) {
-        console.log("API Context Error:", error);
-        setIsLoading(false);
+        const { data, status } = error.response;
+
+        if (status === 500) {
+          alert(data.message);
+          navigate("..");
+          return false;
+        }
+
+        if (status === 401) {
+          alert(data.message);
+          return dispatch(
+            actionUser.save({ accessToken: "", isLoggedIn: data.isLoggedIn })
+          );
+        }
       }
     };
-    fetchProduct();
+
+    // Check path not in Page Login & Register
+    if (!pathsAuth.includes(location.pathname)) {
+      fetchUser();
+    }
   }, []);
 
   return (
     <div className="App">
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, est.
-        Cupiditate a atque laboriosam ab praesentium nostrum molestias,
-        similique sed architecto repellat minima consectetur quo accusantium
-        perspiciatis unde quae deleniti.
-      </h1>
-      {isLoading && (
-        <div>
-          {products.map((p) => (
-            <img src={p.images[0]} alt="" />
-          ))}
-        </div>
-      )}
-      {/* <ScrollTop />
+      <ScrollTop />
       <SidebarMenu />
-      <PopupProduct /> */}
+      <PopupProduct />
 
       {/* Route: All Pages */}
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Home />} />
           <Route path="/shop" element={<Shop />} />
@@ -133,7 +98,7 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
-      </Routes> */}
+      </Routes>
     </div>
   );
 }
